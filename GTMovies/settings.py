097cff360cc5ca8b67e587d104fe33612256899f
ryan <ryanhuang8989@gmail.com>
2025-02-08 19:06:56 -0500

    Updated index.html to extend base.html

--- GTMovies/settings.py
+++ GTMovies/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "GTMovies.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
+        "DIRS": [os.path.join(BASE_DIR, 'GTMovies/templates')]
         ,
         "APP_DIRS": True,
         "OPTIONS": {
